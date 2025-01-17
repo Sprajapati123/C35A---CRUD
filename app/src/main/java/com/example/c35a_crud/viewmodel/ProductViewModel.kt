5f0472e0cfd764dc8395296e33672af51af6f17a
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-17 09:14:42 +0545

    view model

--- app/src/main/java/com/example/c35a_crud/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/c35a_crud/viewmodel/ProductViewModel.kt
@@ -1,4 +1,56 @@
 package com.example.c35a_crud.viewmodel
 
-class ProductViewModel {
+import androidx.lifecycle.MutableLiveData
+import com.example.c35a_crud.model.ProductModel
+import com.example.c35a_crud.repository.ProductRepository
+
+class ProductViewModel(val repo : ProductRepository) {
+    fun addProduct(
+        productModel: ProductModel,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.addProduct(productModel, callback)
+    }
+
+    fun updateProduct(
+        productId: String,
+        data: MutableMap<String, Any>,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.updateProduct(productId, data, callback)
+    }
+
+    fun deleteProduct(
+        productId: String,
+        callback: (Boolean, String) -> Unit
+    ) {
+        repo.deleteProduct(productId, callback)
+    }
+
+    var _products = MutableLiveData<ProductModel>()
+    var products = MutableLiveData<ProductModel>()
+        get() = _products
+
+    var _allProducts = MutableLiveData<List<ProductModel>>()
+    var allProducts = MutableLiveData<List<ProductModel>>()
+        get() = _allProducts
+
+
+    fun getProductById(productId: String){
+        repo.getProductById(productId){
+            products,success,message->
+            if(success){
+                _products.value = products
+            }
+        }
+    }
+
+    fun getAllProduct() {
+        repo.getAllProduct{
+                products,success,message->
+            if(success){
+                _allProducts.value = products
+            }
+        }
+    }
 }
