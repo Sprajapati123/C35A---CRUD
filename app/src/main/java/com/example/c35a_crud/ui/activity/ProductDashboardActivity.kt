ac88b6228054c1dea09927d3d0396f64fb64346c
sprajapati123 <sandisprajapati52@gmail.com>
2025-01-22 08:58:19 +0545

    adapt

--- app/src/main/java/com/example/c35a_crud/ui/activity/ProductDashboardActivity.kt
+++ app/src/main/java/com/example/c35a_crud/ui/activity/ProductDashboardActivity.kt
@@ -6,18 +6,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.c35a_crud.R
+import com.example.c35a_crud.adapter.ProductAdapter
 import com.example.c35a_crud.databinding.ActivityProductDashboardBinding
+import com.example.c35a_crud.repository.ProductRepositoryImpl
+import com.example.c35a_crud.viewmodel.ProductViewModel
+import java.util.ArrayList
 
 class ProductDashboardActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityProductDashboardBinding
+
+    lateinit var productViewModel: ProductViewModel
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityProductDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var repo = ProductRepositoryImpl()
+        productViewModel = ProductViewModel(repo)
+
+        adapter = ProductAdapter(this@ProductDashboardActivity,
+            ArrayList())
+
+        productViewModel.getAllProduct()
+
+        productViewModel.allProducts.observe(this){it->
+            it?.let {
+                adapter.updateData(it)
+            }
+        }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
 
         binding.floatingActionButton.setOnClickListener {
             var intent = Intent(this@ProductDashboardActivity,
